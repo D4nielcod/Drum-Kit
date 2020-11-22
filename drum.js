b60747fa2bf9d4f2dbd08cfb1f6df720a8e2011c
D4nielcod <thorbilol96@gmail.com>
2020-11-22 23:30:36 +0100

    First Commit

---
+++ drum.js
@@ -0,0 +1,38 @@
+// Picture Sounds
+document.getElementById('tom1').onclick = function PlaySound(tomOne) {
+  var snd = new Audio("tom-1.mp3");
+  snd.play();
+}
+
+document.getElementById('tom2').onclick = function PlaySound1(tomTwo) {
+  var snd = new Audio("tom-2.mp3");
+  snd.play();
+}
+
+document.getElementById('tom3').onclick = function PlaySound2(tomThree) {
+  var snd = new Audio("tom-3.mp3");
+  snd.play();
+}
+
+document.getElementById('tom4').onclick = function PlaySound3(tomFour) {
+  var snd = new Audio("tom-4.mp3");
+  snd.play();
+}
+
+document.getElementById('crash').onclick = function PlaySound4(crash) {
+  var snd = new Audio("crash.mp3");
+  snd.play();
+}
+
+document.getElementById('kick').onclick = function PlaySound5(kick) {
+  var snd = new Audio("kick-bass.mp3");
+  snd.play();
+}
+
+document.getElementById('snare').onclick = function PlaySound6(snare) {
+  var snd = new Audio("snare.mp3");
+  snd.play();
+}
+
+
+// keyPress
